d26ca041dc7d68eb1c8ba74312291ddc24591b98
shentajin <phucnttps13928@fpt.edu.vn>
2023-02-12 00:55:40 +0700

    productdetail test

--- src/pages/client/cart/cart.js
+++ src/pages/client/cart/cart.js
@@ -1,31 +1,16 @@
-import React from 'react';
+
+import React from 'react'
 import './cart.css'
 
-function CartItem({ item, onRemove }) {
-  return (
-    <div className="cart-item">
-      <div className="item-details">
-        <h3 className="item-name">{item.name}</h3>
-        <p className="item-price">{item.price}</p>
-      </div>
-      <button className="remove-button" onClick={onRemove}>
-        X
-      </button>
-    </div>
-  );
+
+import './cart.css'
+function Cart(){
+  return(
+ <div>đây là trang cart</div>
+  )
+  
+  
+
 }
 
-function Cart({ items, onRemove }) {
-  return (
-    <div className="cart">
-      <h2 className="header">Giỏ Hàng</h2>
-      <div className="item-list">
-        {items.map((item) => (
-          <CartItem key={item.id} item={item} onRemove={() => onRemove(item.id)} />
-        ))}
-      </div>
-    </div>
-  );
-}
-
-export default Cart;
+export default Cart
--- src/pages/client/productDetail/productDetail.js
+++ src/pages/client/productDetail/productDetail.js
@@ -1,16 +1,64 @@
-import React from 'react';
-import './productDetail.css';
-
-const ProductDetail = (props) => {
+import React from "react";
+import ProductCard from "../../../components/client/Product-card/Productcard";
+import "./productDetail.css";
+import MultiCarousel from "react-multi-carousel";
+import { Link } from "react-router-dom";
+function ProductDetail() {
   return (
-    <div className="product-detail-container">
-      <h1>Tên Sản Phẩm</h1>
-      <div className="product-image">
-        <img src="product.jpg" alt="product" />
+    <div class="product-detail">
+      <div class="image-column">
+        <Link>
+          <img src="/images/nokiac21.png" />
+        </Link>
       </div>
-      <p className="product-description">Mô tả </p>
+      <div class="info-column">
+        <div className="productDetail-price">
+          <p className="new-price">32.990.000đ</p>{" "}
+          <p className="old-price">33.990.000đ</p>
+        </div>
+        <div className="sub-column1"></div>
+        <div className="sub-column1"></div>
+
+        <a href="#" className="sub-column">
+          <strong>16GB 1TB</strong>
+          <span>32.990.000 đ</span>
+        </a>
+        <a href="#" className="sub-column">
+          <strong>8GB 512GB</strong>
+          <span>26.390.000 đ</span>
+        </a>
+        <a href="#" className="sub-column">
+          <strong>6GB 256GB</strong>
+          <span>23.390.000 đ</span>
+        </a>
+        <button class="order-button">
+          <strong>MUA NGAY</strong>
+          <span>(Giao tận nơi hoặc lấy tại cửa hàng)</span>{" "}
+        </button>
+        <button class="tragop-button">
+          <strong class="has-text-white">
+            TRẢ GÓP <span>0%</span>
+          </strong>
+          <br /> <span class="has-text-white">(Xét duyệt qua điện thoại)</span>
+        </button>
+      </div>
+      <div class="details-column">
+        <strong>Thông tin chi tiết</strong>
+        <ul>
+          <li>Kích thước màn hình 6.52 inches</li>
+          <li>Công nghệ màn hình IPS LCD</li>
+          <li>Camera sau 13 MP + 2 MP</li>
+          <li>Camera trước 5 MP</li>
+          <li>Chipset Unisoc SC9863A</li>
+          <li>Dung lượng RAM 6 GB</li>
+          <li>Bộ nhớ trong 256 GB</li>
+          <li>Thẻ SIM 2 SIM (Nano-SIM)</li>
+          <li>Hệ điều hành Android 11 (Go edition)</li>
+          <li>Độ phân giải màn hình 720 x 1600 pixel</li>
+        </ul>
+      </div>
     </div>
   );
-};
+}
 
 export default ProductDetail;
